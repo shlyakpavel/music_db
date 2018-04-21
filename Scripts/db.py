11665cf70a99da3e8fbd15f4ff3f9fc051747865
srkinj <38471171+srkinj@users.noreply.github.com>
2018-04-22 01:56:23 +0300

    Update db.py

--- Scripts/db.py
+++ Scripts/db.py
@@ -20,7 +20,7 @@ def db_form_entry(params, values):
     Args:
         params - parameters used in db
         values - values of these parameters
-        !!!  params and values are collections only
+        !!!  params and values are lists only
     Author: Unknown
     """
     entry = zip(params, values)
@@ -36,7 +36,50 @@ def db_find_strict(db, params):
               param2 : value2... }
             }
     Attention! The entry will be returned only if ALL the params match their values
-    Returns a list of entries(dics) or an empty list# -*- coding: utf-8 -*-
+    Returns a list of entries(dics) or an empty list
+    """
+    found = [] #This list will store all the matching entries
+    for current_entry in db:
+        is_ok = 1;
+        for param in params:
+            if db[current_entry][param] != params[param]:
+                is_ok = 0
+        if is_ok:
+            found.append(db[current_entry])
+    return found;
+        
+    
+
+def db_set_entry_value(entry, param, value):
+    """ This function is used to edit an entry value.
+    Args:
+        entry - database entry
+        param
+        """
+    entry[param] = value
+def db_list_greater_than(db,param,value):
+    """ This function lists all the entries starting from imported value in param
+        value - int
+        param - category of search
+        Returns a list of entries(dics) or an empty list
+        Author: Andrey
+    """
+    found = [] #This list will store all the matching entries
+    for current_entry in db:
+        if int(db[current_entry][param]) >= value:          
+            found.append(db[current_entry])
+    return found;
+        
+
+db = db_create()
+params = ["artist","year"];
+entry = db_form_entry(params,{"String"})
+
+db_add_entry(db,db_form_entry(params,["String","1985"]))
+db_add_entry(db,db_form_entry(params,["String2","1974"]))
+db_add_entry(db,db_form_entry(params,["String2","11"]))
+found = db_find_strict(db, {"artist":"String","year":"1985"})
+found2 = db_list_greater_than(db,"year",1980)# -*- coding: utf-8 -*-
 
 def db_create():
     """
@@ -118,47 +161,3 @@ db_add_entry(db,db_form_entry(params,["String2","1974"]))
 db_add_entry(db,db_form_entry(params,["String2","11"]))
 found = db_find_strict(db, {"artist":"String","year":"1985"})
 found2 = db_list_greater_than(db,"year",1980)
-    """
-    found = [] #This list will store all the matching entries
-    for current_entry in db:
-        is_ok = 1;
-        for param in params:
-            if db[current_entry][param] != params[param]:
-                is_ok = 0
-        if is_ok:
-            found.append(db[current_entry])
-    return found;
-        
-    
-
-def db_set_entry_value(entry, param, value):
-    """ This function is used to edit an entry value.
-    Args:
-        entry - database entry
-        param
-        """
-    entry[param] = value
-def db_find_nonstrict_afterdate(db,year):
-    """ This function lists all the entries starting from imported year
-        year - int
-    
-    """
-    found = [] #This list will store all the matching entries
-    for current_entry in db:
-        is_ok = 0;
-        if int(db[current_entry]["year"]) >= year:
-            is_ok = 1
-        if is_ok:
-            found.append(db[current_entry])
-    return found;
-        
-
-db = db_create()
-params = ["artist","year"];
-entry = db_form_entry(params,{"String"})
-
-db_add_entry(db,db_form_entry(params,["String","1985"]))
-db_add_entry(db,db_form_entry(params,["String2","1974"]))
-db_add_entry(db,db_form_entry(params,["String2","11"]))
-found = db_find_strict(db, {"artist":"String","year":"1985"})
-found2 = db_find_nonstrict_afterdate(db,1980)
