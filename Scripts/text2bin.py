732bd0d217d88f93d5d0c7ca00deffd2ac4a2c13
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2018-04-25 01:24:50 +0300

    CLEANUP

--- Scripts/text2bin.py
+++ Scripts/text2bin.py
@@ -2,6 +2,8 @@
 This is basic module to load database from text file. For test purposal only
 Author: Pavel
     """
+import pickle
+
 #Import all the functions requied to manage db
 from db import db_create
 from db import db_add_entry
@@ -17,3 +19,4 @@ for entry_data in content.split("какаха"):#Спасибо, Маша
     values = entry_data.splitlines()[2::2]
     entry = db_form_entry(params, values)
     db_add_entry(database, entry)
+pickle.dump(database, open("db.pickle", 'wb'))
