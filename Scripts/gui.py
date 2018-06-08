d1f47e33e211c2a22863b0bc2270967e06efdabc
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2018-06-08 18:24:17 +0300

    Docstrings are now avaible for the Application class

--- Scripts/gui.py
+++ Scripts/gui.py
@@ -1,17 +1,26 @@
+"""This module is responsible for GUI
+Everything that is drown by our app should be done here
+However, custom widgets can be stored in external modules
+"""
 import tkinter as tk
 from tkinter import END, BOTH, TOP, YES
 import pickle
 from MultiListbox import MultiListbox
 
-db = pickle.load(open("db.pickle", "rb"))
+DB = pickle.load(open("db.pickle", "rb"))
 
 class Application(tk.Frame):
+    """Main application class.
+    Author: Pavel"""
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
+        """A method to draw the interface
+        Should be only called in class constructor
+        Author: Pavel"""
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
@@ -21,7 +30,7 @@ class Application(tk.Frame):
                               command=ROOT.destroy)
         self.quit.pack(side="bottom")
         self.table = MultiListbox(self, (('Interpreter', 40), ('Song', 20), ('Album', 10)))
-        for i in db.values():
+        for i in DB.values():
             self.table.insert(END,
                               (i['interpreter'], i['song'], i['album']))
         self.table.pack(expand=YES, fill=BOTH, side=TOP)
