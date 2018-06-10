41b8c602a25b9428f3e4bea1483f8291701b34f3
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2018-06-10 23:32:14 +0300

    Mean year in GUI

--- Scripts/stframe.py
+++ Scripts/stframe.py
@@ -3,6 +3,7 @@ from db import db_mean
 class StatsFrame(Frame):
     def __init__(self, master, DB):
         Frame.__init__(self, master, borderwidth=1, relief=SUNKEN)
-        l = Label(master, text="This is window")
+        mean_year = db_mean(DB, 'year')
+        l = Label(master, text="Mean year: %d" % mean_year)
         l.pack(side="top", fill="both", expand=True)
         
