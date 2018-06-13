5e98eb0dad8d2606e5a9d91f810f12a45413d9f2
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2018-06-13 16:50:33 +0300

    Multiple filters

--- Scripts/filter.py
+++ Scripts/filter.py
@@ -1,6 +1,7 @@
 from tkinter import Frame, SUNKEN, Entry, Button, BOTTOM, OptionMenu, \
     StringVar
-from db import db_get_keys, db_list_greater_than, db_create, db_add_entry
+from db import db_get_keys, db_list_greater_than, db_create, db_add_entry, \
+    db_find_strict
 class FilterFrame(Frame):
     """
     Author: Pavel
@@ -8,7 +9,7 @@ class FilterFrame(Frame):
     e = {}
     filter_count = 0
     filters = []
-    
+
     def __init__(self, master, app, DB):
         Frame.__init__(self, master, borderwidth=1, relief=SUNKEN)
         self.database = DB
@@ -21,6 +22,12 @@ class FilterFrame(Frame):
         self.app = app
 
     def draw_filter(self):
+        """Draw a filter in gui
+        It is called once in the beggining
+        It is also called then "+" button is pressed
+        It doesn't set masks or ranges yet #TODO: fix
+        Author: Pavel
+        """
         field = StringVar(self)
         criteria = StringVar(self)
         #variable.set("one") # default value
@@ -32,8 +39,12 @@ class FilterFrame(Frame):
         data_entry.grid(row=self.filter_count, column=3)
         self.filters.append({'field':field, 'criteria': criteria, 'data': data_entry})
         self.filter_count += 1
-        
+
     def apply(self):
+        """This method is called then "apply" button is pressed
+        It is used to process all filters and call their appropriate methods
+        Calls parent window to update data
+        Author: Pavel"""
         new_db = self.database
         for filt in self.filters:
             field = filt['field'].get()
@@ -41,27 +52,32 @@ class FilterFrame(Frame):
             new_db = self.citeria_dict[filt['criteria'].get()](self, new_db, field, data)
         print(new_db)
         self.app.apply_db(new_db)
-            
-            
+
+
     def gt(self, old_db, field, data):
         new_db = db_create()
-        items = db_list_greater_than(self.database, field, int(data))
+        items = db_list_greater_than(old_db, field, int(data))
         for i in items:
             db_add_entry(new_db, i)
         return new_db
-    
+
     def lt(self, old_db, field, data):
         new_db = db_create()
         items = db_list_greater_than(self.database, field, int(data))
         for i in items:
             db_add_entry(new_db, i)
         return new_db
-    
+
     def eq(self, old_db, field, data):
-        print("EQ")
-        
+        """Equality filter
+        Author: Pavel"""
+        new_db = db_create()
+        items = db_find_strict(old_db, {field : data})
+        for i in items:
+            db_add_entry(new_db, old_db[i])
+        return new_db
+
     def inc(self, old_db, field, data):
         print("INC")
-        
+
     citeria_dict = {"Greater then":gt, "Less then": lt, "Is equal to" : eq, "Includes":inc}
-        
